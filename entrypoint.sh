7e253ffdf2e44878ec19c19bbf3669e80fec4573
jayapallion <91729299+jayapallion@users.noreply.github.com>
2023-04-13 20:41:43 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,6 @@ cat "ssh-key"
 mkdir ~/.ssh
 chmod 600 ssh-key
 
-ssh-keyscan -t rsa 3.110.176.123 >> ~/.ssh/known_hosts
-chmod 600 ~/.ssh/known_hosts
+# ssh-keyscan -t rsa 3.110.176.123 >> ~/.ssh/known_hosts
+# chmod 600 ~/.ssh/known_hosts
 ssh -i id_rsa -o StrictHostKeyChecking=no ec2-user@3.110.176.123 "mkdir working"
