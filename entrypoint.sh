a39e785d770577eb0a7d49c0374757e4ebfc681d
jayapallion <91729299+jayapallion@users.noreply.github.com>
2023-04-13 19:10:15 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,5 @@ cat "ssh-key"
 mkdir ~/.ssh
 
 ssh-keyscan -t rsa 3.110.176.123 >> ~/.ssh/known_hosts
+chmod 600 ~/.ssh/known_hosts
 ssh -i ssh-key -o UserKnownHostsFile=~/.ssh/known_hosts -t -t ec2-user@3.110.176.123
