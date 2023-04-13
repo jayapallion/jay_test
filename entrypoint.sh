15c94a2d8b78a2aa99ab28ce12fdb3bd4e7c0b6b
jayapallion <91729299+jayapallion@users.noreply.github.com>
2023-04-13 18:57:35 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,7 @@ echo "$1"
 echo "$2"
 echo "$3"
 cat "ssh-key"
-ssh-keyscan -t rsa 13.233.83.195 >> ~/.ssh/known_hosts
-ssh -t -t -i ssh-key ec2-user@13.233.83.195
+mkdir ~/.ssh
+
+ssh-keyscan -t rsa 3.110.176.123 >> ~/.ssh/known_hosts
+ssh -t -t -i ssh-key ec2-user@3.110.176.123
